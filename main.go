f560fdcf413783bddb61aab00edb8b35b0d8b345
Nikita <vivat404@mail.ru>
2025-02-04 17:54:27 +0300

    ADD HTTP CITY

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"demo/weather/geo"
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("New Proj")
+	city := flag.String("city", "", "Город пользователя")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+	fmt.Println(*city)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(geoData)
+
 }
